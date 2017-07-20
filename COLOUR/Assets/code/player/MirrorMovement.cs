fd37e7b0b9f0cb123230433f074c3cbbbaa6cb47
Pedro Nascimento <pedrovitor.marques@gmail.com>
2017-07-20 03:13:17 -0300

    v.0.2.0
    +Mirrored players
    +Multi-Heighted jump

---
+++ COLOUR/Assets/code/player/MirrorMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorMovement : MonoBehaviour {
+
+	[SerializeField] private Transform _player;
+	private float _offset;
+
+	void Start () {
+		_offset = (transform.position - _player.position).x;
+	}
+
+	void Update () {
+		transform.position = _player.transform.position + new Vector3(_offset, 0);
+	}
+}
--- COLOUR/Assets/code/player/PlayerMovement.cs
+++ COLOUR/Assets/code/player/PlayerMovement.cs
@@ -8,15 +8,26 @@ using UnityEditor;
 [RequireComponent(typeof(Rigidbody2D),typeof(BoxCollider2D))]
 public class PlayerMovement : MonoBehaviour {
 
+	enum JumpState
+	{
+		NONE, DOWN, UP_TIME_OUT, AIR
+	};
+
 	[Header("Movement Properties")]
 	[SerializeField] private float _walkingVelocity;
-	[SerializeField] private float _jumpForce;
+	[SerializeField] private float _jumpMaxForce;
+	[SerializeField] private float _jumpMinForce;
 	[SerializeField] private float _mass;
 	[SerializeField] private float _gravityScale;
 	[Range(.1f,1)]
 	[SerializeField] private float _circleCastRadius;
 	[SerializeField] private Transform _floorDetection;
+	[SerializeField] private float _jumpThreshold;
 	private Rigidbody2D _myRB2D;
+
+	private JumpState _currentJumpState = JumpState.NONE;
+	private float _jumpElapsedTime = 0f;
+	private float _jumpStartTime = 0f;
 
 
 	private void Start(){
@@ -36,9 +47,36 @@ public class PlayerMovement : MonoBehaviour {
 		_myRB2D.AddForce(direction*_walkingVelocity);
 	}
 	private void Jump(){
-		if(Input.GetButtonDown("Jump") && IsGrounded()){
-			_myRB2D.AddForce(Vector2.up*_jumpForce);
+		switch(_currentJumpState)
+		{
+			case JumpState.NONE:
+				if(Input.GetButtonDown("Jump") && IsGrounded()){
+					_currentJumpState = JumpState.DOWN;
+					_jumpStartTime = Time.time;
+				}
+				break;
+			case JumpState.DOWN:
+				_jumpElapsedTime = Time.time - _jumpStartTime;
+				if(!IsGrounded())
+				{
+					_currentJumpState = JumpState.NONE;
+				}			
+				else if(Input.GetButtonUp("Jump") || _jumpElapsedTime >= _jumpThreshold)
+				{
+					_currentJumpState = JumpState.UP_TIME_OUT;
+				}
+				break;
+			case JumpState.UP_TIME_OUT:
+				float jumpForce = _jumpMinForce + (_jumpMaxForce - _jumpMinForce) * _jumpElapsedTime/_jumpThreshold;
+				_myRB2D.AddForce(Vector2.up * jumpForce);
+				_currentJumpState = JumpState.AIR;
+				break;
+			case JumpState.AIR:
+				if(IsGrounded())
+					_currentJumpState = JumpState.NONE;
+				break;
 		}
+
 	}
 	private bool IsGrounded(){
 		RaycastHit2D hit = Physics2D.CircleCast(_floorDetection.position,_circleCastRadius,Vector2.down,_circleCastRadius,1<<LayerMask.NameToLayer("Ground"));
@@ -47,6 +85,8 @@ public class PlayerMovement : MonoBehaviour {
 		}
 		return false;
 	}
+
+
 
 
 
